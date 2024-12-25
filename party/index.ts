6edfd4fdb0fd83d97909b31ffd76f84fb5e93a5e
J Chris Anderson <jchris@fireproof.storage>
2024-12-25 11:39:33 -0800

    partykit server

--- party/index.ts
+++ party/index.ts
@@ -1,30 +1,22 @@
 import type * as Party from "partykit/server";
+import { fireproof } from 'use-fireproof'
+import { connect } from '@fireproof/cloud'
 
 export default class Server implements Party.Server {
   constructor(readonly room: Party.Room) {}
 
-  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.room.id}
-  url: ${new URL(ctx.request.url).pathname}`
-    );
-
-    // let's send a message to the connection
-    conn.send("hello from server");
+  async onStart() {
+    console.log(`server started: ${this.room.id}`)
+    const id = this.room.id
+    const ledger = fireproof(id)
+    connect(ledger)
+    ledger.subscribe((events) => {
+      console.log('ledger event', events)
+    }, true)
   }
 
-  onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.room.broadcast(
-      `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      [sender.id]
-    );
+  async onRequest() {
+    return new Response(`ok: ${this.room.id}`, { status: 200 });
   }
 }
 
